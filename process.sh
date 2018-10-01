7fb1b8d1800ff636df92a2fb4543f7b5f9011f6f
bozhiliu <bozhiliu@email.arizona.edu>
2018-10-01 01:45:12 +0000

    nltk download data

--- process.sh
+++ process.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "Installing packages..."
 apt-get install -qq build-essential python-pip cmake mercurial emacs wget unzip
 pip install --upgrade==9.0.1
-pip install scikit-learn numpy scipy matplotlib ipython jupyter pandas sympy nose nltk tensorflow
+pip install scikit-learn numpy scipy matplotlib ipython jupyter pandas sympy nose nltk tensorflow logging
+
+echo "Getting prebuilt work embeddings"
 cd
 git clone https://github.com/bozhiliu/nlp.git
 cd nlp
@@ -9,3 +12,6 @@ tar -xzf ebm_nlp_1_00.tar.gz
 cd acl_scripts/lstm-crf/
 make glove
 
+python ../../nltk_download.py
+
+python build_data.py
