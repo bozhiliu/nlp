d35e25b77bda6dabdb53d3e3a4c0b463dcdf8e66
bozhiliu <bozhiliu@email.arizona.edu>
2018-10-01 02:23:27 +0000

    remove -qq option for apt-get

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing packages..."
-apt-get install -qq build-essential python-pip cmake mercurial emacs wget unzip
+apt-get install -q -y build-essential python-pip cmake mercurial emacs wget unzip
 pip install --upgrade==9.0.1
 pip install scikit-learn numpy scipy matplotlib ipython jupyter pandas sympy nose nltk tensorflow logging
 
