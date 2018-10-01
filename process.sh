eba903e954fc9d19dbbb5931583590b52169320c
bozhiliu <bozhiliu@email.arizona.edu>
2018-10-01 03:39:02 +0000

    change dir names

--- process.sh
+++ process.sh
@@ -4,10 +4,10 @@ apt-get install -qq -y build-essential python-pip cmake mercurial emacs wget unz
 pip install --upgrade pip==9.0.1
 pip install scikit-learn numpy scipy matplotlib ipython jupyter pandas sympy nose nltk tensorflow logging
 
-echo "Getting prebuilt Glove work embeddings"
-cd
-git clone https://github.com/bozhiliu/nlp.git
-cd nlp
+echo "Getting prebuilt Glove word embeddings"
+cd ~
+git clone https://github.com/bozhiliu/nlp.git EBM-NLP
+cd EBM-NLP
 tar -xzf ebm_nlp_1_00.tar.gz
 cd acl_scripts/lstm-crf/
 make glove
